4390ed112beecbab9dfd91c341f07ad799c8ff61
Ajit Shrestha <ajitshrestha@Ajits-MacBook-Air.local>
2022-11-30 21:27:03 -0600

    Addition work done

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String name;
+    String author;
+    int pages;
+
+// constructor
+public Book (String name, String author, int pages){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+}
+    
+}
